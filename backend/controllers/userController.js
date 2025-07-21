04cc7902e7fbcf26cf740f7c8f4fdc48d66b6b01
Sreedhana <rsreedhana05@gmail.com>
2025-07-21 17:21:00 +0530

    registration of user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler=require("express-async-handler");
-
+const bcrypt=require("bcrypt");
+const User=require("../models/userModels");
 const userLogin=asyncHandler(async(req,res)=>{
     console.log("login done");
     res.status(200);
@@ -7,9 +8,23 @@ const userLogin=asyncHandler(async(req,res)=>{
     
 });
 const userRegister=asyncHandler(async(req,res)=>{
-    console.log("register done");
+    req.body={username,email,password,role};
+    if(!username||!email||!password||!role){
+        res.status(400);
+        throw new Error("all fields are mandatory");
+    }
+    const userExists=User.findOne({email});
+    if(userExists){
+        res.status(409);
+        throw new Error("user already exists");
+    }
+    const hasPassword=bcrypt.hash(password,10);
+    const user=await User.create({
+        email,
+        password:hasPassword,
+        role 
+    });
     res.status(200);
-    res.json({message:"register working"});
-    
+    res.json(user);
 });
 module.exports={userLogin,userRegister};
