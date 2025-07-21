70a1d8932fc317e0ff2c32119c88395300b3f348
Sreedhana <rsreedhana05@gmail.com>
2025-07-21 17:20:34 +0530

    models

---
+++ backend/models/userModels.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongoose");
+const userSchema=mongoose.Schema(
+    {
+        username:{
+            required:[true,"username is required"],
+            type:String
+        },
+        email:{
+            type:String,
+            required:[true,"email is required"],
+            unique:[true,"email already taken"]
+        },
+        password:{
+            type:String,
+            required:[true,"password is required"]
+        },
+        role:{
+            type:String,
+            required:[true,"specify role"]
+        }
+    }
+)
+module.exports=mongoose.model("user",userSchema);
